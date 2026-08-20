b838ba8109ff7182e77e935ae3ce865b36b9eea8
LeHaiDangNguyen3101 <a1914365@adelaide.edu.au>
2026-08-20 14:16:56 +0930

    complete Week 3 Prac B task 2 polymorphism

--- Topic1/Week3/PracB/EmployeePayApp/Program.cs
+++ Topic1/Week3/PracB/EmployeePayApp/Program.cs
@@ -1,29 +1,29 @@
 ﻿using EmployeePayApp;
 
-Console.WriteLine("-- Full-Time Employee --");
+List<Employee> employees = new List<Employee>
+{
+    new FullTimeEmployee("Bill", 50000m),
+    new Contractor("Fred", 50m, 40m)
+};
 
-FullTimeEmployee fullTime = new FullTimeEmployee(
-    "Bill",
-    50000m
-);
+foreach (Employee employee in employees)
+{
+    decimal pay = employee.CalculatePay();
+    decimal tax;
 
-Console.WriteLine($"Name: {fullTime.Name}");
-Console.WriteLine($"Annual salary: ${fullTime.AnnualSalary:F2}");
-Console.WriteLine($"Calculated pay: ${fullTime.CalculatePay():F2}");
-Console.WriteLine(fullTime.GenerateReport());
+    if (employee is FullTimeEmployee fullTime)
+    {
+        tax = fullTime.AnnualSalary * Employee.TaxRate;
+    }
+    else if (employee is Contractor contractor)
+    {
+        decimal grossPay = contractor.Rate * contractor.Hours;
+        tax = grossPay * Employee.TaxRate;
+    }
+    else
+    {
+        tax = 0m;
+    }
 
-Console.WriteLine();
-
-Console.WriteLine("-- Contractor --");
-
-Contractor contractor = new Contractor(
-    "Fred",
-    50m,
-    40m
-);
-
-Console.WriteLine($"Name: {contractor.Name}");
-Console.WriteLine($"Rate: ${contractor.Rate:F2}");
-Console.WriteLine($"Hours: {contractor.Hours}");
-Console.WriteLine($"Calculated pay: ${contractor.CalculatePay():F2}");
-Console.WriteLine(contractor.GenerateReport());
+    Console.WriteLine($"{employee.Name}: Pay ${pay:F2}, Tax ${tax:F2}");
+}
